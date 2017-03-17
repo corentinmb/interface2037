6a39c1d1068e6546f7099acb75fa8cf9f4b07136
Corentin MOREAU <corentin.moreau2@gmail.com>
2017-03-17 10:17:03 +0100

    je répare mes erreurs

--- ServeurQuestion/src/main/java/composants/entitees/Question.java
+++ ServeurQuestion/src/main/java/composants/entitees/Question.java
@@ -2,11 +2,7 @@ package composants.entitees;
 
 import java.util.Date;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 @Entity
