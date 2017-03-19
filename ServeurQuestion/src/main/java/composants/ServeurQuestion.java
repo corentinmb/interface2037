578cb61b5c8bdd7e70d4656447de83add7809506
Corentin MOREAU <corentin.moreau2@gmail.com>
2017-03-19 09:09:07 +0100

    ajout thymeleaf

--- ServeurQuestion/src/main/java/composants/ServeurQuestion.java
+++ ServeurQuestion/src/main/java/composants/ServeurQuestion.java
@@ -1,10 +1,13 @@
 package composants;
 
 
+import composants.entitees.Question;
+import composants.entitees.QuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
 
@@ -12,10 +15,19 @@ import springfox.documentation.annotations.ApiIgnore;
 @ApiIgnore
 @SpringBootApplication
 public class ServeurQuestion {
+    @Autowired
+    QuestionRepository questionRepository;
 
     @RequestMapping("/")
     String home() {
-        return "index.html";
+        return "index";
+    }
+
+    @RequestMapping("/details/{id}")
+    public String details(@PathVariable("id") Long id, Model model) {
+        Question q = questionRepository.findOne(id);
+        model.addAttribute("q", q);
+        return "details";
     }
 
     public static void main(String[] args) throws Exception {
--- ServeurQuestion/src/main/java/composants/controleurs/QuestionController.java
+++ ServeurQuestion/src/main/java/composants/controleurs/QuestionController.java
@@ -1,38 +1,39 @@
-package composants.entitees;
+package composants.controleurs;
 
+import composants.entitees.Question;
+import composants.entitees.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.Date;
 
 @RestController
 @RequestMapping("/questions")
 public class QuestionController {
     @Autowired
-    private QuestionDAO questionDAO;
+    private QuestionRepository questionRepository;
 
     @RequestMapping(method = RequestMethod.GET)
     Collection<Question> getQuestions(){
-        return questionDAO.findAll();
+        return questionRepository.findAll();
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/{id}")
     Question getQuestion(@PathVariable Long id){
-        return questionDAO.findOne(id);
+        return questionRepository.findOne(id);
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/last")
     Question getLastQuestion(){
-        return Collections.max(questionDAO.findAll(), Comparator.comparing(c -> c.getDatecreation()));
+        return Collections.max(questionRepository.findAll(), Comparator.comparing(c -> c.getDatecreation()));
     }
 
     @RequestMapping(method = RequestMethod.POST)
     public Question addQuestion(@RequestParam(value = "libelle", required = false) String libelle) {
         Question q = new Question(libelle);
-        return questionDAO.saveAndFlush(q);
+        return questionRepository.saveAndFlush(q);
     }
 
     /*@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
